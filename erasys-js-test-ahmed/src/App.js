380f69ee9b41b54ffb70668234526a3f443927bc
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-07-19 20:43:14 +0200

    DEV-01: project setup

---
+++ erasys-js-test-ahmed/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Route, Redirect, Switch } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <Switch>
+    </Switch>
+  );
+}
+
+export default App;
